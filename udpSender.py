b0c22a4cc42924f432a829d2408c837ee480d9a2
tom-sloan-school <55495754+tom-sloan-school@users.noreply.github.com>
2019-09-18 11:05:05 -0400

    Update udpSender.py

--- udpSender.py
+++ udpSender.py
@@ -1,16 +1,20 @@
-# Source: https://pymotw.com/2/socket/udp.html
 
 import socket, sys, time
 
 host = sys.argv[1]
 textport = sys.argv[2]
-data = sys.argv[3]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = (host, port)
 
 while 1:
+    print ("Enter how many messages you'd like to send: ENTER to quit")
+    data = sys.stdin.readline().strip()
+   
+    if not len(data):
+        break
+#    s.sendall(data.encode('utf-8'))
     data_int = int(data)
     message = "Message"
     counter = 1;
@@ -18,6 +22,12 @@ while 1:
         sendMessage = message + str(counter)
         counter = counter + 1
         s.sendto(sendMessage.encode('utf-8'), server_address)
+
+        
+        print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
+
+        buf, address = s.recvfrom(port)
+            
         print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
 
 s.shutdown(1)

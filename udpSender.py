2c157e9e57eeb44a47f6fb27b9b3505f4e113d2f
jakebridge99 <55495383+jakebridge99@users.noreply.github.com>
2019-09-18 09:43:20 -0400

    Change to send n messages
    Changed custom message to n number of messages

--- udpSender.py
+++ udpSender.py
@@ -10,12 +10,18 @@ port = int(textport)
 server_address = (host, port)
 
 while 1:
-    print ("Enter data to transmit: ENTER to quit")
+    print ("Enter how many messages you'd like to send: ENTER to quit")
     data = sys.stdin.readline().strip()
     if not len(data):
         break
 #    s.sendall(data.encode('utf-8'))
-    s.sendto(data.encode('utf-8'), server_address)
+
+    message = "Message"
+    counter = 1;
+    while counter <= data:
+        sendMessage = message + counter
+        counter = counter + 1
+        s.sendto(sendMessage.encode('utf-8'), server_address)
 
 s.shutdown(1)
 

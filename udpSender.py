454d9fcfff0f3d0fb60fbb93ed97f1fc2d41a640
jakebridge99 <55495383+jakebridge99@users.noreply.github.com>
2019-09-18 11:09:23 -0400

    Update udpSender.py

--- udpSender.py
+++ udpSender.py
@@ -10,24 +10,17 @@ port = int(textport)
 server_address = (host, port)
 
 while 1:
-   
-    if not len(data):
-        break
 #    s.sendall(data.encode('utf-8'))
     data_int = int(data)
     message = "Message"
     counter = 1;
     while counter <= data_int:
-        sendMessage = message + str(counter)
-        counter = counter + 1
-        s.sendto(sendMessage.encode('utf-8'), server_address)
-
-        
-        print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
-
-        buf, address = s.recvfrom(port)
-            
-        print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
+         sendMessage = message + str(counter)
+         counter = counter + 1
+         s.sendto(sendMessage.encode('utf-8'), server_address)
+         print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
+         buf, address = s.recvfrom(port)
+         print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
 
 s.shutdown(1)
 

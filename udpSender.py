072c69d7e1296b5ec772a1a6f931fa087737adcd
tom-sloan-school <55495754+tom-sloan-school@users.noreply.github.com>
2019-09-18 09:49:28 -0400

    Update udpSender.py
    fixing error

--- udpSender.py
+++ udpSender.py
@@ -12,13 +12,14 @@ server_address = (host, port)
 while 1:
     print ("Enter how many messages you'd like to send: ENTER to quit")
     data = sys.stdin.readline().strip()
+   
     if not len(data):
         break
 #    s.sendall(data.encode('utf-8'))
-
+    data_int = int(data)
     message = "Message"
     counter = 1;
-    while counter <= data:
+    while counter <= data_int:
         sendMessage = message + counter
         counter = counter + 1
         s.sendto(sendMessage.encode('utf-8'), server_address)

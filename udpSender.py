4a8f903809c319aadb2b3dcaafa3414f2250b71f
jakebridge99 <55495383+jakebridge99@users.noreply.github.com>
2019-09-18 10:30:11 -0400

    Update udpSender.py

--- udpSender.py
+++ udpSender.py
@@ -23,6 +23,14 @@ while 1:
         sendMessage = message + str(counter)
         counter = counter + 1
         s.sendto(sendMessage.encode('utf-8'), server_address)
+        while True:
+
+            print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
+
+            buf, address = s.recvfrom(port)
+                if not len(buf):
+            break
+            print ("Received %s bytes from %s %s: " % (len(buf), address, buf ))
 
 s.shutdown(1)
 
